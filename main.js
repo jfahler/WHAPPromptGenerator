aa9ed6b0c00e8dbfa17e30a7848e604f6eb76870
jfahler <jfahler@gmail.com>
2020-07-14 00:24:23 -0500

    Version 1 with HTML, CSS, JS - first iteration of JS only creates SAQs

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-function themes() {
-  let whThemes = ['environmental', 'cultural', 'political', 'economic', 'social', 'technological']; 
-  let randomizedTheme = whThemes[Math.floor(Math.random() * whThemes.length)]; 
-  return randomizedTheme; 
-}; 
+const output = document.querySelector('.prompt_output'); 
 
-function skills() {
-  let whSkills = ['similarity', 'difference', 'change', 'continue', 'cause', 'effect']; 
-  let randomizedSkill = whSkills[Math.floor(Math.random() * whSkills.length)]; 
-  return randomizedSkill; 
-}; 
-
-function regions() {
-  let whRegions = ['North America', 'Latin America', 'Africa', 'Eurasia', 'Oceania', 'East Asia', 'Southeast Asia', 'South Asia']; 
-  let randomizedRegion = whRegions[Math.floor(Math.random() * whRegions.length)]; 
-  return randomizedRegion; 
-}; 
-
-
-function periods() {
-  let whPeriods = ['1200-1450', '1450-1750', '1750-1900', '1900-Present']; 
-  let randomizedPeriod = whPeriods[Math.floor(Math.random() * whPeriods.length)]; 
-  return randomizedPeriod; 
-}; 
+let whThemes = ['environmental', 'cultural', 'political', 'economic', 'social', 'technological']; 
+let whSkills = ['similarity', 'difference', 'change', 'continuity', 'cause', 'effect']; 
+let whRegions = ['North America', 'Latin America', 'Africa', 'Eurasia', 'Oceania', 'East Asia', 'Southeast Asia', 'South Asia']; 
+let whPeriods = ['1200-1450', '1450-1750', '1750-1900', '1900-Present']; 
 
 function generateTopic() {
-  console.log("Evaluate ONE " + themes() +' ' + skills() + ' ' + regions() + ' ' + periods())
-}
+  const newPrompt = document.createElement('p'); 
+  let randomizedTheme = whThemes[Math.floor(Math.random() * whThemes.length)]; 
+  let randomizedSkill = whSkills[Math.floor(Math.random() * whSkills.length)]; 
+  let randomizedRegion = whRegions[Math.floor(Math.random() * whRegions.length)];
+  let randomizedPeriod = whPeriods[Math.floor(Math.random() * whPeriods.length)]; 
 
+  let finalOutut = newPrompt.textContent; 
+  document.frmTest.txtTest.value = 'Evaluate ONE ' + randomizedTheme +' ' + randomizedSkill + ' in ' + randomizedRegion + ' during the period circa ' + randomizedPeriod + '.'; 
+  //output.appendChild(newPrompt); 
+}
