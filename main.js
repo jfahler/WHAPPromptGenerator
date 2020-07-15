b18b9605ce5912d92caf9cb600a7acf2d2a793e8
jfahler <jfahler@gmail.com>
2020-07-15 18:18:31 -0500

    More mobile compatibility and better DOM manipulation.

--- main.js
+++ main.js
@@ -2,16 +2,28 @@ const output = document.querySelector('.output_box');
 
 let whThemes = ['environmental', 'cultural', 'political', 'economic', 'social', 'technological']; 
 let whSkills = ['similarity', 'difference', 'change', 'continuity', 'cause', 'effect']; 
+let pdThreeContent = ['global revolutions', 'the industrial revolution', 'new imperialism', 'responses to European imperialism']; 
 let whRegions = ['North America', 'Latin America', 'Africa', 'Eurasia', 'Oceania', 'East Asia', 'Southeast Asia', 'South Asia']; 
-let whPeriods = ['1200-1450', '1450-1750', '1750-1900', '1900-Present']; 
+let whPeriods = ['1750-1900']; 
+//Remember to re-add other date ranges!
 
-function generateTopic() {
-  const newPrompt = document.createElement('p'); 
+
+function generateSAQ() {
+  const newPrompt = document.querySelector('.saq_output_box'); 
   let randomizedTheme = whThemes[Math.floor(Math.random() * whThemes.length)]; 
   let randomizedSkill = whSkills[Math.floor(Math.random() * whSkills.length)]; 
+  let randomizedPdThreeContent = pdThreeContent[Math.floor(Math.random() * pdThreeContent.length)]; 
   let randomizedRegion = whRegions[Math.floor(Math.random() * whRegions.length)];
   let randomizedPeriod = whPeriods[Math.floor(Math.random() * whPeriods.length)];  
-  newPrompt.textContent = 'Evaluate ONE ' + randomizedTheme +' ' + randomizedSkill + ' in ' + randomizedRegion + ' during the period circa ' + randomizedPeriod + '.'; 
-  output.appendChild(newPrompt); 
-  
+  newPrompt.textContent = 'Evaluate ONE ' + randomizedTheme +' ' + randomizedSkill + ' of ' + randomizedPdThreeContent + ' in ' + randomizedRegion + ' during the period circa ' + randomizedPeriod + '.'; 
+}
+
+function generateLEQ() {
+  const newLeq = document.querySelector('.leq_output_box'); 
+  let randomizedTheme = whThemes[Math.floor(Math.random() * whThemes.length)]; 
+  let randomizedSkill = whSkills[Math.floor(Math.random() * whSkills.length)]; 
+  let randomizedPdThreeContent = pdThreeContent[Math.floor(Math.random() * pdThreeContent.length)]; 
+  let randomizedRegion = whRegions[Math.floor(Math.random() * whRegions.length)];
+  let randomizedPeriod = whPeriods[Math.floor(Math.random() * whPeriods.length)];  
+  newLeq.textContent = 'Develop an argument that evaluates the extent to which ' + randomizedTheme +' ' + randomizedSkill + ' of ' + randomizedPdThreeContent + ' in ' + randomizedRegion + ' during the period circa ' + randomizedPeriod + '.'; 
 }
